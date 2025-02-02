66397514e38363d10983950c2fd5b54e79e84d1e
Emmanuel Siamoonga <emmanuelsiamoonga@gmail.com>
2025-02-03 01:53:54 +0200

    commit

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         System.out.println("==============  WELCOME TO EMMANUEL'S ATM INTERFACE SYSTEM =============");
         Scanner data = new Scanner(System.in);
         System.out.println("PLEASE REGISTER BELOW");
@@ -13,12 +13,12 @@ public class App {
         String password = data.next();
         System.out.print("Confirm Password: ");
         String password2 = data.next();
+        String account_number = "720012345115";
+        String[] userDetails = {account_number,reg_name,phone,password,password2};
+        int pass1 = Integer.parseInt(password);
+        int pass2 = Integer.parseInt(password2);
         if(password.length()==5){
-            int pass1 = Integer.parseInt(password);
-            int pass2 = Integer.parseInt(password2);
             if(pass1==pass2){
-                String account_number = "720012345115";
-                
                 viewDetails(reg_name,phone,account_number);
             }
             else if(!(password==password2)){
@@ -28,15 +28,46 @@ public class App {
         else{
             System.out.println("PLEASE ENSURE PASSWORD CONTAINS 5 DIGITS ");
         }
+       
+        //LOGIN PART
+        System.out.println("YOU CAN NOW LOGIN");
+        System.out.print("Enter Account Number: ");
+        int loginAccountNumber = data.nextInt();
+        System.out.print("Enter Password: ");
+        int login_password = data.nextInt();
+        int loginAccount = Integer.parseInt(account_number);
+     
+        if(loginAccount==loginAccountNumber){
+            if(login_password==pass1){
+                System.out.println("WELCOME "+reg_name);
+                menu();
+            }
+            else{
+                System.out.println("PLEASE ENSURE YOU ENTER THE CORRECT PASSWORD");
+            }
+        }
+        else{
+            System.out.println("INVALID ACCOUNT NUMBER ");
+        }
         data.close();
     }
 
     //FUNCTION TO CONFIRM DETAILS
     public static void viewDetails(String name,String phone,String account_number){
         System.out.println("===================================================================");
-        System.out.println("=\t\tPLEASE CONFIRM DETAILS BELOW");
-        System.out.println("=\t\tNAME: "+name.toUpperCase());
-        System.out.println("=\t\tPHONE: "+phone);
-        System.out.println("=\t\tACCOUNT NUMBER: "+account_number);
+        System.out.println("PLEASE CONFIRM DETAILS BELOW");
+        System.out.println("===================================================================");
+        System.out.println("NAME: "+name.toUpperCase());
+        System.out.println("PHONE: "+phone);
+        System.out.println("ACCOUNT NUMBER: "+account_number);
+        System.out.println("=====================================================================");
+    }
+       
+    //MAIN MENU METHOD
+    public static void menu(){
+        System.out.println("1. WITHDRAW");
+        System.out.println("2 DEPOSIT");
+        System.out.println("3. CHECK BALANCE");
+        System.out.println("4. EXIT");
     }
 }
