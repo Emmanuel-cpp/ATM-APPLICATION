8b46a06ac18da94d9dae608589fa37cded9bba16
Emmanuel Siamoonga <emmanuelsiamoonga@gmail.com>
2025-02-05 11:31:44 +0200

    THIS IS MY GIT ADDITION

--- src/ATM.java
+++ src/ATM.java
@@ -3,13 +3,29 @@ public class ATM {
     private double withDrawAmount;
     private double depositAmount;
 
-    //GETTERS AND SETTERS
+    //GETTERS AND SETTERS FOR BALANCE
     public void setBalance(double balance){
         this.balance=balance;
     }
     public double getBalance(){
         return balance;
     }
+    //GETTER AND SETTERS FOR WITHDRAWAMOUNT
+    public void setWithdraw(double withDrawAmount){
+        this.withDrawAmount=withDrawAmount;
+    }
+    public double getWithDrawAmount(){
+        return withDrawAmount;
+    }
+    //GETTER AND SETRER FOR DEPOSIT AMOUNT
+    public void getDeposit(double depositAmount){
+        this.depositAmount=depositAmount;
+    }
+    public double getDeposit(){
+        return depositAmount;
+    }
+
+
 
     
 
